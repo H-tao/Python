5e71367ac97805f178cc5957373da81a2653e62f
H-tao <jixingjiukeyile@163.com>
2021-01-12 21:05:47 +0800

    Create 无序链表.py

---
+++ 数据结构/无序链表.py
@@ -0,0 +1,59 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self, new_data):
+        self.data = new_data
+
+    def setNext(self, new_next):
+        self.next = new_next
+
+class UnorderedList:
+    def __init__(self):
+        self.head = None
+
+    def add(self, item):
+        """ 头插法 """
+        temp = Node(item)
+        temp.setNext(self.head)
+        self.head = temp
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current is not None:
+            count = count + 1
+            current = current.getNext()
+        return count
+
+    def search(self, item):
+        current = self.head
+        found = False
+        while current is not None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+        return found
+
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+        if previous is None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
